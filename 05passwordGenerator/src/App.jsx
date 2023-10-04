1a5d0ba7802616a4711c1094c7ffe3794521b6c2
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2023-10-04 14:21:33 +0000

    add

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='text-4xl text-center'>Password Generator</h1>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
     </>
   )
 }
